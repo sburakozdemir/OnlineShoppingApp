7d2292688acff1e67c072a630173d7b2183f72cf
sburakozdemir <sburakozdemir00@gmail.com>
2024-10-25 14:45:46 +0300

    DbContext oluşturuldu ,appsettings ve program.cs'te gerekli işlemler yapıldı

---
+++ OnlineShoppingApp.Data/Context/OnlineShoppingAppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShoppingApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingApp.Data.Context
+{
+    public class OnlineShoppingAppDbContext: DbContext  
+    {
+        public OnlineShoppingAppDbContext(DbContextOptions<OnlineShoppingAppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+        public DbSet<OrderProductEntity> OrderProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // UserEntity konfigürasyonu
+            modelBuilder.Entity<UserEntity>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            // OrderProductEntity konfigürasyonu
+            modelBuilder.Entity<OrderProductEntity>()
+                .HasKey(op => new { op.OrderId, op.ProductId });
+
+            modelBuilder.Entity<OrderProductEntity>()
+                .HasOne(op => op.Order)
+                .WithMany(o => o.OrderProducts)
+                .HasForeignKey(op => op.OrderId);
+
+            modelBuilder.Entity<OrderProductEntity>()
+                .HasOne(op => op.Product)
+                .WithMany(p => p.OrderProducts)
+                .HasForeignKey(op => op.ProductId);
+        }
+    }
+}
--- OnlineShoppingApp.WebApi/Program.cs
+++ OnlineShoppingApp.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineShoppingApp.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("default");
+
+builder.Services.AddDbContext<OnlineShoppingAppDbContext>(options => options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

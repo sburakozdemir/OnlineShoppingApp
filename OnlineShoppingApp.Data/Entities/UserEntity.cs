2e940a5543a2f45a28043b988d37f9be1f420d33
sburakozdemir <sburakozdemir00@gmail.com>
2024-10-25 13:19:02 +0300

    ProductEntity,UserType ve UserEntity eklendi.

---
+++ OnlineShoppingApp.Data/Entities/UserEntity.cs
@@ -0,0 +1,20 @@
+﻿using OnlineShoppingApp.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingApp.Data.Entities
+{
+    public class UserEntity:BaseEntity
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public UserType UserType { get; set; }
+    }
+}
